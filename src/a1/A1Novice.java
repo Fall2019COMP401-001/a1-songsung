2e3212863f2f92d1bd847caa0b2565c6912514fe
Sung <kowynsong@gmail.com>
2019-08-27 15:00:57 -0400

    attempt 1

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -7,8 +7,21 @@ public class A1Novice {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-
-		// Your code follows here.
-		
+		int numOfCustomers = scan.nextInt();
+		double customerTotal = 0;
+		for (int howManyCustomers = 0; howManyCustomers < numOfCustomers; howManyCustomers++) {
+			String firstName = scan.next();
+			String lastName = scan.next();
+			int numOfItems = scan.nextInt();
+			for (int howManyItems = 0; howManyItems < numOfItems; howManyItems++) {
+				int numOfEachItem = scan.nextInt();
+				String itemBought = scan.next();
+				double priceOfItem = scan.nextDouble();
+				customerTotal = customerTotal + numOfEachItem * priceOfItem;
+			}
+			String stringCustomerTotal = String.format("%.2f", customerTotal);
+			System.out.println(firstName.charAt(0) + ". " + lastName + ": " + stringCustomerTotal);
+			customerTotal = 0;
+		}
 	}
 }
